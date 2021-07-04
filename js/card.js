12e56e01799b968ca297534edcea14ca15e15853
Ben Myers <benl.myers01@gmail.com>
2021-07-04 11:26:44 -0400

    🎬 Add second card + mockup

--- js/card.js
+++ js/card.js
@@ -9,8 +9,15 @@ window.onscroll = function() {
 function scrollRotate() {
   let card1 = document.getElementById("card1");
   card1.style.transform = "translate(-50%,-50%) rotate3d(1, 1, 0, " + cardRotation1() + "deg) rotate(10deg)";
+  card2.style.transform = "translate(-50%, 0%) rotate3d(-1, 1, 0, " + cardRotation2() + "deg) rotate(-10deg)";
 }
 
+let speed = 7.5;
+
 function cardRotation1() {
-  return 7.5 + 10* window.pageYOffset / 20.0;
+  return speed + 10 * window.pageYOffset / 20.0;
+}
+
+function cardRotation2() {
+  return speed + 10 * window.pageYOffset / 15.0;
 }
